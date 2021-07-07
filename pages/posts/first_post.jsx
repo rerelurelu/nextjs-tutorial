4267e7dc324ba31fe3465d4b97f8f2065f28a808
zoniha <awaawapurin@gmail.com>
2021-07-07 20:49:03 +0900

    ✨ feat: Complete lesson-3

--- pages/posts/first_post.jsx
+++ pages/posts/first_post.jsx
@@ -1,15 +1,21 @@
 import Link from 'next/link';
+import Head from 'next/head';
+
+import { Layout } from '../../components/index';
 
 const FirstPost = () => {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>Good title!</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 };
 
